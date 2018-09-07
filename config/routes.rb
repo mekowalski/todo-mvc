6762877ea9c7329bde45bb93dbdc882fbd0c8f07
Malind Kowalski <malind.nikulski@gmail.com>
2018-09-07 15:16:20 -0600

    destroy site con, modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :lists
-  get 'site/index'
+  get 'list/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'site#index'
+  root 'lists#index'
 end
