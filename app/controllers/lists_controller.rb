5f8ec9eef1bf246331532c0999c3ebc4e1b6fa19
Malind Kowalski <malind.nikulski@gmail.com>
2018-09-10 15:06:44 -0600

    add comments to controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,7 +1,7 @@
 class ListsController < ApplicationController
 
   def index
-    @list = List.new
+    @list = List.new #this instance variable is completely different from @create instantiation
     @lists = List.all
   end
 
@@ -10,11 +10,12 @@ class ListsController < ApplicationController
   end
 
   def create
-    @list = List.new
+    @list = List.new #objs in request cycle only exist in per one action per request
     @list.name = params[:list][:name]
     @list.save
 
-    redirect_to list_path(@list)
+    redirect_to list_path(@list )
   end
 
+  #every action renders in the context of one http request
 end
