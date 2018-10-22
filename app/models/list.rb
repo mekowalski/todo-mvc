e76856c6092522584d63016b705c6f1dfffdfe20
Malind Kowalski <malind.nikulski@gmail.com>
2018-10-22 13:11:49 -0600

    modify items#create, validate items, remove comment and create same placeholder for item

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,5 +2,4 @@ class List < ApplicationRecord
   has_many :items
 
   validates :name, presence: :name
-  #presence: :true also works properly but this is a new way for me and seems cleaner for me personally
 end
