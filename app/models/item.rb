56be4495ed083b92655bbcdc6082dffc827306d8
Malind Kowalski <malind.nikulski@gmail.com>
2018-10-22 11:03:39 -0600

    remove comments from table and att notes to model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,5 @@
 class Item < ApplicationRecord
+  belongs_to :list
+  #macros are very magical
+  #adding a whole bunch of methods
 end
--- db/migrate/20181022164901_create_items.rb
+++ db/migrate/20181022164901_create_items.rb
@@ -3,8 +3,7 @@ class CreateItems < ActiveRecord::Migration[5.2]
     create_table :items do |t|
       t.string :description
       t.integer :list_id
-      #wiring the schema the DB
-      #wire the model with association
+
       t.timestamps
     end
   end
