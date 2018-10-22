38ea3ec9f481e19652c1a725feddd3c393dee281
Malind Kowalski <malind.nikulski@gmail.com>
2018-10-22 15:02:42 -0600

    FINALLY fixed checkbox issue! create model methods

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,17 @@ class Item < ApplicationRecord
   belongs_to :list
 
   validates :description, presence: :description
+
+  STATUS = {
+    :incomplete => 0,
+    :complete => 1
+  }
+
+  def complete?
+    self.status == STATUS[:complete]
+  end
+
+  def incomplete?
+    self.status == STATUS[:incomplete]
+  end
 end
