fe16c4503d3497270105c6bd41b7921744c53742
Malind Kowalski <malind.nikulski@gmail.com>
2018-10-22 14:21:45 -0600

    add status to items table

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_22_164901) do
+ActiveRecord::Schema.define(version: 2018_10_22_202047) do
 
   create_table "items", force: :cascade do |t|
     t.string "description"
     t.integer "list_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status"
   end
 
   create_table "lists", force: :cascade do |t|
