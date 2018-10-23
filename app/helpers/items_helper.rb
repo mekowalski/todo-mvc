2e1ba670772cf5e889dcbfb0fb42459989c8fe67
Malind Kowalski <malind.nikulski@gmail.com>
2018-10-23 14:39:08 -0600

    refactor even more of the content tage for in the helper

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -2,4 +2,11 @@ module ItemsHelper
   def li_class_for_item(item)
     'completed' if item.complete?
   end
+
+  def li_for_item(item)
+    content_tag_for :li, item, class: li_class_for_item(item) do
+    #what goes in here, can't inject all the html in here
+      yield
+    end
+  end
 end
