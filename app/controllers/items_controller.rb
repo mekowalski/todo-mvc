786d52e985b29a52cee0b4947953d67e71603690
Malind Kowalski <malind.nikulski@gmail.com>
2018-10-23 13:06:39 -0600

    remove comments, refactor #update

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,16 +10,14 @@ class ItemsController < ApplicationController
   end
 
   def update
-    #what Item and i trying to update
     @item = Item.find(params[:id])
-    @item.status = params[:item][:status] #0 or 1
-    @item.save
+    @item.update(item_params)
 
     redirect_to list_path(@item.list)
   end
 
   private
   def item_params
-    params.require(:item).permit(:description)
+    params.require(:item).permit(:description, :status)
   end
 end
