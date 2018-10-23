2a65807f565d603f20649d7ce3453a69ae970715
Malind Kowalski <malind.nikulski@gmail.com>
2018-10-23 13:37:11 -0600

    create items helper, refactor

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,10 @@
 module ItemsHelper
+  def li_class_for_items(item)
+    'completed' if item.complete?
+  end
 end
+
+#same as form_for, link_to
+#little methods that return html or strings to be used in any place in views dir
+#actionview iterates through all modules to define (in helpers dir) and automatically mixes them into
+#actionview scope regardless of what view is rendered
