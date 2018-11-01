b99758555153c464909e630538509a8d8160e336
Malind Kowalski <malind.nikulski@gmail.com>
2018-11-01 13:01:59 -0600

    add user email and migreate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_22_202047) do
+ActiveRecord::Schema.define(version: 2018_11_01_185734) do
 
   create_table "items", force: :cascade do |t|
     t.string "description"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 2018_10_22_202047) do
 
   create_table "lists", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
