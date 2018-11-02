f04bbd249848fe8d3087d55a9f1fc138beea47ce
Malind Kowalski <malind.nikulski@gmail.com>
2018-11-02 17:16:44 -0600

    password field for login and signup, authenticate user login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new
     @user.email = params[:user][:email]
+    @user.password = params[:user][:password]
     if @user.save
+      session[:user_id] = @user.id
       redirect_to root_path
     else
       render :new
