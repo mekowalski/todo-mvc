0fbb0d31bfc1740eb13bb609b1f8f5aca1b8ef35
Malind Kowalski <malind.nikulski@gmail.com>
2018-11-02 14:08:05 -0600

    generate sessions con, map routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'lists#index'
   get 'items/create'
+  get '/login', to: 'sessions#new'
+  post '/sessions', to: 'sessions#create'
 
   resources :users, only: [:new, :create]
 
