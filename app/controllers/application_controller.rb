fed39f0e27ea7a8783372ef9010c08e9fafc6a16
Malind Kowalski <malind.nikulski@gmail.com>
2018-11-02 16:54:12 -0600

    create current user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,11 @@ class ApplicationController < ActionController::Base
   end
 
   def logged_in?
-    session[:user_id]
+    !!current_user
   end
+
+  def current_user
+    @current_user ||=User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current
 end
