f41980b0c3b9f7ddcf6c43a9e4fd5c526c72c762
Malind Kowalski <malind.nikulski@gmail.com>
2018-11-08 14:53:39 -0700

    create model method called in sessions#create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,19 +4,9 @@ class SessionsController < ApplicationController
 
   def create
     if auth_hash = request.env['omniauth.auth']
-      oauth_email = request.env['omniauth.auth']['info']['email']
-      if user = User.find_by(email: oauth_email)
-        session[:user_id] = user.id
-        redirect_to root_path
-      else
-        user = User.create(email: oauth_email, password: SecureRandom.hex)
-        if user.save
-          session[:user_id] = user.id
-          redirect_to root_path
-        else
-          raise user.errors.full_messages.inspect
-        end
-      end
+      user = User.find_or_create_by_omniauth(auth_hash)
+      session[:user_id] = user.id
+      redirect_to root_path
     else
       user = User.find_by(email: params[:email])
       if user && user.authenticate(params[:password])
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ApplicationRecord
   has_secure_password
-  
+
   validates :email, presence: :true
   validates :email, uniqueness: :true
+
+  def self.find_or_create_by_omniauth(auth_hash)
+    oauth_email = auth_hash['info']['email']
+    if user = User.find_by(email: oauth_email)
+      return user
+    else
+      user = User.create(email: oauth_email, password: SecureRandom.hex)
+    end
+  end
 end
