8807118939bb84a040d766726308ab44c9d70466
Malind Kowalski <malind.nikulski@gmail.com>
2018-11-08 15:03:07 -0700

    refactor model method further

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,8 @@ class User < ApplicationRecord
   validates :email, uniqueness: :true
 
   def self.find_or_create_by_omniauth(auth_hash)
-    oauth_email = auth_hash['info']['email']
-    if user = User.find_by(email: oauth_email)
-      return user
-    else
-      user = User.create(email: oauth_email, password: SecureRandom.hex)
+    self.where(email: auth_hash['info']['email']).first_or_create do |user| #always return a user instance
+      user.password = SecureRandom.hex
     end
   end
 end
