1e1416f796ad79094994f433b67f901a7f6c0f43
Malind Kowalski <malind.nikulski@gmail.com>
2018-12-14 14:53:51 -0700

    cleaning code

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -4,6 +4,18 @@ function Item(attributes) {
   this.id = attributes.id
 }
 
+//instantiates new item on the json
+Item.success = function(json) {
+  var item = new Item(json)
+  var itemLi = item.renderLi()
+
+  $('ul.todo-list').append(itemLi)
+}
+
+Item.error = function(response) {
+  console.log('You broke it', response)
+}
+
 //this gives an error where Item.templateSource is undefined
 //needs to be in document ready, so the doc can load and then run correctly
 $(function() {
@@ -16,28 +28,25 @@ Item.prototype.renderLi = function() {
   return Item.template(this)
 }
 
-$(function() {
-  $('form#new_item').on('submit', function(e) {
-    e.preventDefault()
-    var $form = $(this)
-    var action = $form.attr('action')
-    var params = $form.serialize()
+Item.formSubmit = function(e) {
+  e.preventDefault()
+  var $form = $(this)
+  var action = $form.attr('action')
+  var params = $form.serialize()
 
-    $.ajax({
-      url: action,
-      data: params,
-      dataType: 'application/json',
-      method: 'POST',
-      accepts: 'application/json'
-    })
-    .always(function(response) {
-      json = JSON.parse(response.responseText)
-      var item = new Item(json)
-      var itemLi = item.renderLi()
-
-      $('ul.todo-list').append(itemLi)
-    })
+  $.ajax({
+    url: action,
+    data: params,
+    dataType: 'json',
+    method: 'POST'
+    // accepts: 'application/json'
   })
+  .success(Item.success)
+  .error(Item.success)
+}
+
+$(function() {
+  $('form#new_item').on('submit', Item.formSubmit)
 })
 
 //check out these:
