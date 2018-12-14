307f07e5f3b04a93e8cfaec3a6c0169266519676
Malind Kowalski <malind.nikulski@gmail.com>
2018-12-13 18:47:33 -0700

    get data

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -1,6 +1,12 @@
 $(function() {
   $('form#new_item').on('submit', function(e) {
     e.preventDefault()
-    alert('This is working') //this did work
+    //cache objects, when object references jquery object,
+    //THIS always refers to the item that triggered the event(the entire form)
+    var $form = $(this)
+    var action = $form.attr('action')
+    var params = $form.serialize()
+
+    $.post(action, params)
   })
 })
